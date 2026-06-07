8705f81b8a6a92763ea8c3a4fbbc00ae0f0606e7
DevRafaelSM <rafael.mix.8952@gmail.com>
2026-06-07 15:30:15 -0300

    feat: primeira migration e correção na instalação do drf

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from produtos.models import Produto
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "preco", "ativo", "criado_em", "atualizado_em")
+    search_fields = ["nome"]
+    list_filter = ["ativo"]
+
+
+admin.site.register(Produto, ProdutoAdmin)
--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    descricao = models.TextField(max_length=1000, default="", blank=True)
+    preco = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=False,
+        validators=[MinValueValidator(0.01)],
+    )
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
